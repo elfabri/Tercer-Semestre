ce1228ea784cd8a7ef09c60719e063c4525d325d
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-04-21 15:00:52 -0300

    Lección 02 video 9

--- Tecnicatura3Py/Leccion02/Archivos/leer_archivo.py
+++ Tecnicatura3Py/Leccion02/Archivos/leer_archivo.py
@@ -7,20 +7,29 @@
 
 archivo = open("prueba.txt", 'r', encoding="utf-8")
 
-print(archivo.read(16))  # lee hasta caracter 16
-print(archivo.read(10))  # continua leyendo hasta caracter 26 en este caso
+# print(archivo.read(16))  # lee hasta caracter 16
+# print(archivo.read(10))  # continua leyendo hasta caracter 26 en este caso
 
-print(archivo.readline())  # lee primera línea completa
-print(archivo.readline())  # lee segunda línea completa
+# print(archivo.readline())  # lee primera línea completa
+# print(archivo.readline())  # lee segunda línea completa
 
 # Iteración
-for linea in archivo:
-    print(linea)
+# for linea in archivo:
+    # print(linea)
 
 # Creación de lista de líneas del archivo
 # se consigue con archivo.readlines()
-print(archivo.readlines())
+# print(archivo.readlines())
 
 # se puede acceder al elemento n con archivo.readlines()[n]
 # se puede limitar las lineas en la lista a primeras
 # n lineas con archivo.readlines(n) con n mayor igual que 1
+
+# Anexamos información, copiamos a otro
+archivo2 = open("copia.txt", 'a', encoding="utf-8")
+archivo2.write(archivo.read())
+archivo.close()
+archivo2.close()
+
+# archivo2 en modo append, este programa fue ejecutado 2 veces
+print("se ha terminado el proceso de leer y copiar archivos")
