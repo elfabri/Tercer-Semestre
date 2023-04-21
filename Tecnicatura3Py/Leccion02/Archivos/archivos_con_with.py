8328bf03cff8e8f132fdcd37f2e6549a555b7dd2
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-04-21 15:23:04 -0300

    Lección02 video 12

--- Tecnicatura3Py/Leccion02/Archivos/archivos_con_with.py
+++ Tecnicatura3Py/Leccion02/Archivos/archivos_con_with.py
@@ -1,7 +1,15 @@
 # Contexto with - manejo de contexto with
-with open("prueba.txt", 'r', encoding="utf-8") as archivo:
-    print(archivo.read())
+# with open("prueba.txt", 'r', encoding="utf-8") as archivo:
+    # print(archivo.read())
 
 # los métodos que utiliza son:
 # __enter__ abre
 # __exit__ cierra
+
+# Manejo de archivos con clase prehecha que define los métodos
+# __enter__ para abrir archivo en modo lectura
+# __exit__ para cerrar el mismo
+from manejo_archivos_2 import manejo_archivos_2
+
+with manejo_archivos_2("prueba.txt") as archivo:  # se ejecuta __enter__
+    print(archivo.read())  # se imprime, y a continuación se ejecuta __exit__
