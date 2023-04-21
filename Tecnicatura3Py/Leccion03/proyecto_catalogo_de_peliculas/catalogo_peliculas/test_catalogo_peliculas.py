f3e6459defec82912ca4405d51092ec4108bc539
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-04-21 16:31:02 -0300

    Lección 03 video 5

--- Tecnicatura3Py/Leccion03/proyecto_catalogo_de_peliculas/catalogo_peliculas/test_catalogo_peliculas.py
+++ Tecnicatura3Py/Leccion03/proyecto_catalogo_de_peliculas/catalogo_peliculas/test_catalogo_peliculas.py
@@ -1,3 +1,6 @@
+from dominio.Pelicula import Pelicula
+from servicio.catalogo_peliculas import CatalogoPeliculas as cp
+
 opcion = None
 
 while opcion != 4:
@@ -9,8 +12,21 @@ while opcion != 4:
         print("4. Salir ")
 
         opcion = int(input("Introduzca una opción de menú (1-4): "))
+
+        if opcion == 1:
+            nombre_pelicula = input("Ingrese el nombre de la película: ")
+            pelicula = Pelicula(nombre_pelicula)
+            cp.agregar_peliculas(pelicula)
+
+        elif opcion == 2:
+            cp.listar_peliculas()
+
+        elif opcion == 3:
+            cp.eliminar_pelicula()
+
     except Exception as e:
         print(f"Ocurrió un error de tipo: {e}")
         opcion = None
+
     else:
         print("Salimos del programa")
