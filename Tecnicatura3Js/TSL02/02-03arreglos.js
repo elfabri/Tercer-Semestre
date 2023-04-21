2401f0becb53f40168c8d489fae209c301c130a1
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-04-21 18:51:55 -0300

    Lección 02 video 3

--- Tecnicatura3Js/TSL02/02-03arreglos.js
+++ Tecnicatura3Js/TSL02/02-03arreglos.js
@@ -11,3 +11,19 @@ console.log(autos[2]);
 for (let i = 0; i < autos.length; i++) {
     console.log(i + ': ' + autos[i]);
 }
+
+// Modificamos los elementos del arreglo
+autos[1] = "Volvo";
+console.log(autos[1]);
+
+// Agregamos nuevos valores al arreglo
+autos.push("Audi");
+console.log(autos);
+
+// Otras formas de agregar elementos al arreglo
+autos[autos.length] = "Porche";
+console.log(autos);
+
+// Tercera forma de agregar elementos tenendo cuidado
+autos[6] = "Renault";
+console.log(autos); // No tira error sino que rellena con undefined
