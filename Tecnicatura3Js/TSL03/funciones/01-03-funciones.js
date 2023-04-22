864fbe34337965e33dc012ef98c4a70074ed50ef
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-04-22 20:18:33 -0300

    Lección 03 - Video 9

--- Tecnicatura3Js/TSL03/funciones/01-03-funciones.js
+++ Tecnicatura3Js/TSL03/funciones/01-03-funciones.js
@@ -66,3 +66,12 @@ function sumarTodo() {
     }
     return suma;
 }
+
+// Paso por valor
+// Tipos primitivos
+let k = 10;
+function cambiarValor(a) {
+    a = 20; // variable de uso dentro de la función
+}
+
+cambiarValor(k); // le pasamos solo el valor de k, no la referencia a k, no se va a modificar el valor de k
