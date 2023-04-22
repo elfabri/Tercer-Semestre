d38bc2019dc0ad6a52d0f1d49ce1b041b34fdf58
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-04-22 19:43:09 -0300

    Lección 03 - Video 3

--- Tecnicatura3Js/TSL03/funciones/01-03-funciones.js
+++ Tecnicatura3Js/TSL03/funciones/01-03-funciones.js
@@ -10,3 +10,8 @@ miFuncion(5, 4);
 
 let resultado = miFuncion(6, 7);
 console.log(resultado);
+
+// función de tipo expresión o anónima
+let x = function(a, b) { return a + b };
+resultado = x(5, 6);
+console.log(resultado);
