2b52d1c00cdd341fd2931c7bc8cff4f169ab4edc
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-04-26 19:37:18 -0300

    video 01

---
+++ Tecnicatura3Js/TSL04/01-04-objetos.js
@@ -0,0 +1,13 @@
+let x = 10;
+
+console.log(x.length); // undefined
+
+// Objeto
+let persona = {
+    nombre: 'Carlos',
+    apellido: 'A',
+    email: 'a@a.a',
+    edad: 3
+}
+
+console.log(persona.nombre);
