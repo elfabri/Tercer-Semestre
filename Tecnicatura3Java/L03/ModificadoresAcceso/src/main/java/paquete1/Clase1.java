6b983c68ea7cd5fbdd1e26adbc0011e145ad84dc
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-04-30 23:53:04 -0300

    video 07 protected

--- Tecnicatura3Java/L03/ModificadoresAcceso/src/main/java/paquete1/Clase1.java
+++ Tecnicatura3Java/L03/ModificadoresAcceso/src/main/java/paquete1/Clase1.java
@@ -7,12 +7,21 @@ public class Clase1 {
      *      clases de otros paquetes
      */
     public String atributoPublic = "Valor atributo public";
+    protected String atributoProtected = "valor atributo protected";
 
     public Clase1() {
         System.out.println("Constructor Público");
     }
 
+    protected Clase1(String atributoPublic) {
+        System.out.println("Constructor Protected");
+    }
+
     public void metodoPublico() {
         System.out.println("Método public");
+    }
+
+    public void metodoProtected() {
+        System.out.println("Método protected");
     }
 }
