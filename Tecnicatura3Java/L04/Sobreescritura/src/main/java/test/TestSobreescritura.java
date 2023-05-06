d3117eabdb5aff86af5f488129ab52de1b36e0c8
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-05-06 13:50:38 -0300

    video 04

--- Tecnicatura3Java/L04/Sobreescritura/src/main/java/test/TestSobreescritura.java
+++ Tecnicatura3Java/L04/Sobreescritura/src/main/java/test/TestSobreescritura.java
@@ -1,10 +1,19 @@
 package test;
 
-import domain.Gerente;
+import domain.*;
 
 public class TestSobreescritura {
     public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Juanita", 10000);
+        imprimir(empleado1);
+        // System.out.println("empleado1: " + empleado1.obtenerDetalles());
+
         Gerente gerente1 = new Gerente("Josefína", 5000, "Sistemas");
-        System.out.println("gerente1: " + gerente1.obtenerDetalles());
+        imprimir(gerente1); // Entra como tipo empleado gracias a la herencia con la clase Empleado
+        // System.out.println("gerente1: " + gerente1.obtenerDetalles());
+    }
+
+    public static void imprimir(Empleado empleado) {
+        System.out.println("Empleado: " + empleado.obtenerDetalles());
     }
 }
