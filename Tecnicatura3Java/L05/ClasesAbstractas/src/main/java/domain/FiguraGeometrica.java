ee2688906f8d3efe45085f8525323e893bff4020
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-05-12 16:20:24 -0300

    video 7 Clases abstractas p2

--- Tecnicatura3Java/L05/ClasesAbstractas/src/main/java/domain/FiguraGeometrica.java
+++ Tecnicatura3Java/L05/ClasesAbstractas/src/main/java/domain/FiguraGeometrica.java
@@ -1,6 +1,25 @@
 package domain;
 
 public abstract class FiguraGeometrica {
+    protected String tipoFigura;
+
+    protected FiguraGeometrica(String tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
     // La clase padre no define comportamiento
     abstract void dibujar(); 
+
+    public String getTipoFigura() {
+        return tipoFigura;
+    }
+
+    public void setTipoFigura(String tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
+    @Override
+    public String toString() {
+        return "FiguraGeometrica { " + tipoFigura + " }";
+    }
 }
--- Tecnicatura3Java/L05/ClasesAbstractas/src/main/java/domain/Rectangulo.java
+++ Tecnicatura3Java/L05/ClasesAbstractas/src/main/java/domain/Rectangulo.java
@@ -1,8 +1,12 @@
 package domain;
 
 public class Rectangulo extends FiguraGeometrica {
-    void dibujar() {
-        // Comportamiento de la subclase
+    public Rectangulo (String tipoFigura) {
+        super(tipoFigura);
+    }
 
+    @Override
+    public void dibujar() {
+        System.out.println("Se imprime un: " + this.getClass().getSimpleName());
     }
 }
