8be154019a9091b26dcfa9f91eace40075b54c37
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-05-29 19:45:16 -0300

    L08 - video 03 - Creación de la clase conexion Parte 1

---
+++ Tecnicatura3Py/L07/capa_datos_persona/conexion.py
@@ -0,0 +1,8 @@
+class Conexion:
+    _DATABASE = "test_bd"
+    _USERNAME = "postgres"
+    _PASSWORD = "admin"
+    _HOST = "127.0.0.1"
+    _DB_PORT = "5432"
+    _conexion = None
+    _cursor = None
