b7fe6702e1535ddfc1d2e094d6cd934207d0753b
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-05-29 19:57:34 -0300

    L08 - v 3 - Creación de la clase conexion Parte 2

--- Tecnicatura3Py/L07/capa_datos_persona/conexion.py
+++ Tecnicatura3Py/L07/capa_datos_persona/conexion.py
@@ -1,3 +1,8 @@
+import psycopg2 as bd
+from logger_base import log
+import sys
+
+
 class Conexion:
     _DATABASE = "test_bd"
     _USERNAME = "postgres"
@@ -6,3 +11,22 @@ class Conexion:
     _DB_PORT = "5432"
     _conexion = None
     _cursor = None
+
+    @classmethod
+    def obtenerConexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion = bd.connect(
+                        host=cls._HOST,
+                        port=cls._DB_PORT,
+                        user=cls._USERNAME,
+                        password=cls._PASSWORD,
+                        database=cls._DATABASE,
+                        )
+                log.debug(f"Conexión exitosa: {cls._conexion}")
+                return cls._conexion
+            except Exception as e:
+                log.error(f"Error: {e}")
+                sys.exit()
+            else:
+                return cls._conexion
