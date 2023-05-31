b157da08e3956dcbe7392c014e359cd244c2d32e
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-05-31 20:48:46 -0300

    C8 - video 7 - Creamos excepción parte 1

---
+++ Tecnicatura3Java/L08/ManejoExcepciones1/src/main/java/excepciones/OperacionExcepcion.java
@@ -0,0 +1,7 @@
+package excepciones;
+
+public class OperacionExcepcion extends Exception {
+    public OperacionExcepcion (String msg) {
+        super(msg);
+    }
+}
--- Tecnicatura3Java/L08/ManejoExcepciones1/src/main/java/test/TestExcepciones.java
+++ Tecnicatura3Java/L08/ManejoExcepciones1/src/main/java/test/TestExcepciones.java
@@ -3,12 +3,12 @@ package test;
 public class TestExcepciones {
     public static void main( String[] args ) {
         int resultado = 0;
-        // try {
+        try {
             resultado = 10/0;
-        // } catch (Exception e) {
-            // System.out.println("Ocurrió un error");
-            // e.printStackTrace(System.out);
-        // }
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error");
+            e.printStackTrace(System.out);
+        }
         System.out.println("resultado = " + resultado);
     }
 }
