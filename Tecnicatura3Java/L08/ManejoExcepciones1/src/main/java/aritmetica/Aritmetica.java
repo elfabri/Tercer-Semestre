01f8c3b956393640294c097f0ba359e04cc7e62b
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-05-31 22:48:55 -0300

    C8 - video 10 - Runtime Exceptions

--- Tecnicatura3Java/L08/ManejoExcepciones1/src/main/java/aritmetica/Aritmetica.java
+++ Tecnicatura3Java/L08/ManejoExcepciones1/src/main/java/aritmetica/Aritmetica.java
@@ -3,8 +3,7 @@ package aritmetica;
 import excepciones.OperacionExcepcion;
 
 public class Aritmetica {
-    public static int division(int numer, int denom)
-            throws OperacionExcepcion {
+    public static int division(int numer, int denom) {
         if (denom == 0) {
             throw new OperacionExcepcion("División entre cero");
         }
--- Tecnicatura3Java/L08/ManejoExcepciones1/src/main/java/excepciones/OperacionExcepcion.java
+++ Tecnicatura3Java/L08/ManejoExcepciones1/src/main/java/excepciones/OperacionExcepcion.java
@@ -1,6 +1,6 @@
 package excepciones;
 
-public class OperacionExcepcion extends Exception {
+public class OperacionExcepcion extends RuntimeException {
     public OperacionExcepcion (String msg) {
         super(msg);
     }
