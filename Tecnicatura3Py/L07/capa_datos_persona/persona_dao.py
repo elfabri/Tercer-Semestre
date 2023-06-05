ccd13257c67c10fb9ebb5931ebf296602aa3bae7
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-06-05 18:33:01 -0300

    C9 - v1 - Método seleccionar en la clase PersonaDAO

--- Tecnicatura3Py/L07/capa_datos_persona/persona_dao.py
+++ Tecnicatura3Py/L07/capa_datos_persona/persona_dao.py
@@ -1,3 +1,7 @@
+from conexion import Conexion
+from Persona import Persona
+
+
 class PersonaDAO:
     """
     Data Access Object
@@ -7,3 +11,15 @@ class PersonaDAO:
     _INSERTAR = "INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)"
     _ACTUALIZAR = "UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE \"idPersona\"=%s"
     _ELIMINAR = "DELETE FROM persona WHERE \"idPersona\"=%s"
+
+    # definimos los métodos de clase
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall
+                personas = []
+                for r in registros:
+                    p = Persona(r[0], r[1], r[2], r[3])
+                    personas.append(p)
