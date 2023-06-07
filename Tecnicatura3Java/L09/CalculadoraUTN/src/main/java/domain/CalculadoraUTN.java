eff80a24bb55f714dc62c4ed057c2b33425b8252
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-06-07 20:04:20 -0300

    C9 - v3 - Ponemos entrada de información

--- Tecnicatura3Java/L09/CalculadoraUTN/src/main/java/domain/CalculadoraUTN.java
+++ Tecnicatura3Java/L09/CalculadoraUTN/src/main/java/domain/CalculadoraUTN.java
@@ -1,12 +1,19 @@
 package domain;  // En el del profe, no hace un package
 
+import java.util.Scanner;
+
 public class CalculadoraUTN {
     public static void main( String[] args ) {
+        Scanner entrada = new Scanner(System.in);
 
         System.out.println("** App Calcu **");
 
-        int ope1 = 10;  // se debería usar var
-        int ope2 = 20;  // se debería usar var
+        System.out.print("Ingrese primer número: ");
+        int ope1 = Integer.parseInt(entrada.nextLine());  // se debería usar var
+                                                          //
+        System.out.print("Ingrese segundo número: ");
+        int ope2 = Integer.parseInt(entrada.nextLine());;  // se debería usar var
+
         int resultado = ope1 + ope2;  // se debería usar var
 
         System.out.println("Resultado = " + resultado);
