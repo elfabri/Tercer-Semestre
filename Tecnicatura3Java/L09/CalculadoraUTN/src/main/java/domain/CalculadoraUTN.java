bde81e8fc303384e8093616d0c652a480ff3ad20
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-06-07 19:52:33 -0300

    C9 - v2 - definiendo las variables (v1 instala intellij y crea archivo sin modificarlo)

---
+++ Tecnicatura3Java/L09/CalculadoraUTN/src/main/java/domain/CalculadoraUTN.java
@@ -0,0 +1,14 @@
+package domain;  // En el del profe, no hace un package
+
+public class CalculadoraUTN {
+    public static void main( String[] args ) {
+
+        System.out.println("** App Calcu **");
+
+        int ope1 = 10;  // se debería usar var
+        int ope2 = 20;  // se debería usar var
+        int resultado = ope1 + ope2;  // se debería usar var
+
+        System.out.println("Resultado = " + resultado);
+    }
+}
