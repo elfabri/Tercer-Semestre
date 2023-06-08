5f8e72fb469e3700e6eada881cb1d26180325e8d
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-06-08 16:23:49 -0300

    C9 - v6 - Ciclo y salida con break

--- Tecnicatura3Java/L09/CalculadoraUTN/src/main/java/CalculadoraUTN.java
+++ Tecnicatura3Java/L09/CalculadoraUTN/src/main/java/CalculadoraUTN.java
@@ -4,50 +4,54 @@ public class CalculadoraUTN {
     public static void main( String[] args ) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("** App Calcu **");
-// Mostramos menú
-        System.out.println("""
-            1. Suma
-            2. Resta
-            3. Multiplicación
-            4. División
-            5. Salir
-        """);
+        while (true) {
 
-        System.out.print("Operación a realizar? ");
-        int ope = Integer.parseInt(entrada.nextLine());
+            System.out.println("\n** App Calcu **");
+            // Mostramos menú
+            System.out.println("""
+                    1. Suma
+                    2. Resta
+                    3. Multiplicación
+                    4. División
+                    5. Salir
+                    """);
 
-        if (ope >= 1 && ope <= 4) {
-            System.out.print("Ingrese primer número: ");
-            var ope1 = Integer.parseInt(entrada.nextLine());
+            System.out.print("Operación a realizar? ");
+            int ope = Integer.parseInt(entrada.nextLine());
 
-            System.out.print("Ingrese segundo número: ");
-            var ope2 = Integer.parseInt(entrada.nextLine());
+            if (ope >= 1 && ope <= 4) {
+                System.out.print("Ingrese primer número: ");
+                var ope1 = Integer.parseInt(entrada.nextLine());
 
-            int res;
-            switch (ope) {
-                case 1 -> {
-                    res = ope1 + ope2;
-                    System.out.print("Resultado de la suma: " + res);
+                System.out.print("Ingrese segundo número: ");
+                var ope2 = Integer.parseInt(entrada.nextLine());
 
-                } case 2 -> {
-                    res = ope1 - ope2;
-                    System.out.print("Resultado de la resta: " + res);
-                    
-                } case 3 -> {
-                    res = ope1 * ope2;
-                    System.out.print("Resultado de la multiplicación: " + res);
+                int res;
+                switch (ope) {
+                    case 1 -> {
+                        res = ope1 + ope2;
+                        System.out.println("Resultado de la suma: " + res);
 
-                } case 4 -> {
-                    res = ope1 / ope2;
-                    System.out.print("Resultado de la división: " + res);
+                    } case 2 -> {
+                        res = ope1 - ope2;
+                        System.out.println("Resultado de la resta: " + res);
 
-                } default -> System.out.println("Opción invalida");
-            }
-        } else if (ope == 5) {
+                    } case 3 -> {
+                        res = ope1 * ope2;
+                        System.out.println("Resultado de la multiplicación: " + res);
+
+                    } case 4 -> {
+                        res = ope1 / ope2;
+                        System.out.println("Resultado de la división: " + res);
+
+                    } default -> System.out.println("Opción invalida");
+                }
+            } else if (ope == 5) {
                 System.out.println("Saliendo del programa");
-        } else {
-            System.out.println("Opción invalida");
+                break;
+            } else {
+                System.out.println("Opción invalida");
+            }
         }
     }
 }
