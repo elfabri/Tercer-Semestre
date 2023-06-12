e5f60326acb40946e72fd9e2a990fa68663de5a7
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-06-12 18:45:26 -0300

    C10 - v2 - pool de conexiones con python y postgres parte 2 (v1 solo charla)

--- Tecnicatura3Py/L07/capa_datos_persona/conexion.py
+++ Tecnicatura3Py/L07/capa_datos_persona/conexion.py
@@ -1,4 +1,4 @@
-import psycopg2 as bd
+from psycopg2 import pool
 from logger_base import log
 import sys
 
@@ -9,42 +9,24 @@ class Conexion:
     _PASSWORD = "admin"
     _HOST = "127.0.0.1"
     _DB_PORT = "5432"
-    _conexion = None
-    _cursor = None
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None
 
     @classmethod
     def obtenerConexion(cls):
-        if cls._conexion is None:
-            try:
-                cls._conexion = bd.connect(
-                        host=cls._HOST,
-                        port=cls._DB_PORT,
-                        user=cls._USERNAME,
-                        password=cls._PASSWORD,
-                        database=cls._DATABASE,
-                        )
-                log.debug(f"Conexión exitosa: {cls._conexion}")
-                return cls._conexion
-            except Exception as e:
-                log.error(f"Error: {e}")
-                sys.exit()
-        else:  # En este commit, arregló la identación de esto
-            return cls._conexion
+        pass
 
     @classmethod
     def obtenerCursor(cls):
-        if cls._cursor is None:
+        pass
+
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
             try:
-                cls._cursor = cls.obtenerConexion().cursor()
-                log.debug(f"Se abrió cursor {cls._cursor}")
-                return cls._cursor
-            except Exception as e:
-                log.error(f"Error: {e}")
-                sys.exit()
-        else:  # En este commit, arregló la identación de esto
-            return cls._cursor
+                cls._pool = pool.SimpleConnectionPool()
 
 
 if (__name__ == "__main__"):
-    Conexion.obtenerConexion()
-    Conexion.obtenerCursor()
+    pass
