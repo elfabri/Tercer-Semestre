9926863c9142f7db5dff4b0c882d13244e3b704e
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-06-12 18:51:59 -0300

    C10 - v3 - Obtener conexión a partir del pool parte 1

--- Tecnicatura3Py/L07/capa_datos_persona/conexion.py
+++ Tecnicatura3Py/L07/capa_datos_persona/conexion.py
@@ -25,7 +25,19 @@ class Conexion:
     def obtenerPool(cls):
         if cls._pool is None:
             try:
-                cls._pool = pool.SimpleConnectionPool()
+                cls._pool = pool.SimpleConnectionPool(cls._MIN_CON,
+                                                      cls._MAX_CON,
+                                                      host=cls._HOST,
+                                                      user=cls._USERNAME,
+                                                      password=cls._PASSWORD,
+                                                      port=cls._DB_PORT,
+                                                      database=cls._DATABASE)
+                log.debug(f"Creación de pool exitosa: {cls._pool}")
+            except Exception as e:
+                log.error(f"Ocurrió un error al opbtener el pool: {e}")
+                sys.exit()
+        else:
+            return cls._pool
 
 
 if (__name__ == "__main__"):
