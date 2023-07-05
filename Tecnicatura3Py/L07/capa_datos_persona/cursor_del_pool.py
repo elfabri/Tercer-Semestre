45f9037017f5d9909e01326d903148e5f47ed927
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-07-05 16:49:29 -0300

    C11 - v4 - Creación clase cursor del pool parte 2

--- Tecnicatura3Py/L07/capa_datos_persona/cursor_del_pool.py
+++ Tecnicatura3Py/L07/capa_datos_persona/cursor_del_pool.py
@@ -12,3 +12,17 @@ class CursorDelPool:
         self._conexion = Conexion.obtenerConexion
         self._cursor = self._conexion.cursor()
         return self._cursor
+
+    def __exit__(self, tipo_exception, valor_exception, detalle_exception):
+        log.debug("Se ejecuta el método exit")
+
+        if valor_exception:
+            self._conexion.rollback()
+            log.debug(f"Ocurrió un error: {valor_exception}")
+
+        else:
+            self._conexion.commit()
+            log.debug("Commit de la transacción")
+
+        self._cursor.close()
+        Conexion.liberarConexion(self._conexion)
