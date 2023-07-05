a83ad571efdf417856cbc4e9a8414ebf761241fa
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-07-05 16:16:29 -0300

    C11 - v1 - método liberar conexión

--- Tecnicatura3Py/L07/capa_datos_persona/conexion.py
+++ Tecnicatura3Py/L07/capa_datos_persona/conexion.py
@@ -41,6 +41,11 @@ class Conexion:
         else:
             return cls._pool
 
+    @classmethod
+    def liberarConexion(cls, conexion):
+        cls.obtenerPool().putconn(conexion)
+        log.debug(f"Regresamos la conexión del pool: {conexion}")
+
 
 if (__name__ == "__main__"):
     conexion1 = Conexion.obtenerConexion()
