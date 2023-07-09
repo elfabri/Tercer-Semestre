699114daffaebe5151bb50f4a6fb7ffaa383d1c5
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-07-09 20:37:32 -0300

    12.2 - Listado de estudiantes con la Clase EstudianteDAO - Parte 2

--- Tecnicatura3Java/L12/estudiante/src/main/java/UTN/datos/EstudianteDao.java
+++ Tecnicatura3Java/L12/estudiante/src/main/java/UTN/datos/EstudianteDao.java
@@ -1,6 +1,8 @@
 package UTN.datos;
 
+import static UTN.conexion.Conexion.getConnection;
 import java.util.List;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -14,5 +16,9 @@ public class EstudianteDao {
         // Creamos algunos objetos necesarios para comunicarnos con la base de datos 
         PreparedStatement ps; // Envía la sentencia
         ResultSet rs;  // Obtiene el resultado
+
+        // Creamos objeto de tipo conexión
+        Connection con = getConnection();
+        String sql = "SELECT * FROM estudiantes2022;";
     }
 }
