d664326b5b2e104c2a0e09f527942113e2885e08
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-07-09 20:16:59 -0300

    12.1 - Creamos la clase entidad Estudiante

--- Tecnicatura3Java/L12/estudiante/src/main/java/UTN/Main.java
+++ Tecnicatura3Java/L12/estudiante/src/main/java/UTN/Main.java
@@ -2,11 +2,14 @@ package UTN;
 
 import UTN.conexion.Conexion;
 
-public class main {
-    public static void main( String[] args ) {
+public class Main {
+    public static void main(String[] args) {
+
         var conexion = Conexion.getConnection();
+
         if (conexion != null) {
             System.out.println("Conexión exitosa: " + conexion);
+
         } else {
             System.out.println("Hubo un error al conectarse");
         }
