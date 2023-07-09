32f2929cd2d9d91beee12f9451fca5134c329b1b
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-07-09 20:55:32 -0300

    12.2 - Listado de estudiantes con la Clase EstudianteDAO - Parte 4

--- Tecnicatura3Java/L12/estudiante/src/main/java/UTN/datos/EstudianteDao.java
+++ Tecnicatura3Java/L12/estudiante/src/main/java/UTN/datos/EstudianteDao.java
@@ -21,17 +21,34 @@ public class EstudianteDao {
         // Creamos objeto de tipo conexión
         Connection con = getConnection();
         String sql = "SELECT * FROM estudiantes ORDER BY idestudiantes2022;";
+
         try {
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
+
             while (rs.next()) {
                 var estudiante = new Estudiante();
+
                 estudiante.setIdEstudiante(rs.getInt("idestudiante2022"));
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+
+                // Falta agregarlo a la lista
+                estudiantes.add(estudiante);
             }
+
         } catch (Exception e) {
             System.out.println("Ocurrió un error al seleccionar datos: " + e.getMessage());
         } finally {
-
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Ocurrió un error al cerrar la conexión: " + e.getMessage());
+            }
         }
+
+        return estudiantes;
     }
 }
